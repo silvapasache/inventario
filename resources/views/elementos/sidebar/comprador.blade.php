963d33a1a632f27ab6fb7b5d44891908ef25ad4a
SilvaLuis94 <wuils.sc.18@gmail.com>
2020-10-10 22:11:57 -0500

    actualizado menus

--- resources/views/elementos/sidebar/comprador.blade.php
+++ resources/views/elementos/sidebar/comprador.blade.php
@@ -2,23 +2,17 @@
       <div id="sidebar" class="nav-collapse ">
         <!-- sidebar menu start-->
         <ul class="sidebar-menu">
-          <li class="active">
+          <li class="">
             <a class="" href="{{route('desboard')}}">
                           <i class="icon_house_alt"></i>
                           <span>Desboard</span>
                       </a>
           </li>
-          <li class="sub-menu">
-            <a href="javascript:;" class="">
+          <li class="">
+            <a href="{{route('proveedores.index')}}" class="">
                           <i class="icon_desktop"></i>
                           <span>Proveedor</span>
-                          <span class="menu-arrow arrow_carrot-right"></span>
-                      </a>
-            <ul class="sub">
-              <li><a class="" href="general.html">Components</a></li>
-              <li><a class="" href="buttons.html">Buttons</a></li>
-              <li><a class="" href="grids.html">Grids</a></li>
-            </ul>
+            </a>
           </li>
           
 
@@ -34,21 +28,6 @@
                           <i class="fa fa-star"></i>
                           <span>Productos</span>
             </a>
-          </li>
-
-          <li class="sub-menu">
-            <a href="javascript:;" class="">
-                          <i class="icon_documents_alt"></i>
-                          <span>Pages</span>
-                          <span class="menu-arrow arrow_carrot-right"></span>
-                      </a>
-            <ul class="sub">
-              <li><a class="" href="profile.html">Profile</a></li>
-              <li><a class="" href="login.html"><span>Login Page</span></a></li>
-              <li><a class="" href="contact.html"><span>Contact Page</span></a></li>
-              <li><a class="" href="blank.html">Blank Page</a></li>
-              <li><a class="" href="404.html">404 Error</a></li>
-            </ul>
           </li>
 
         </ul>
